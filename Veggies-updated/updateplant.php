65385c9d57e8167252a8e4f6f5ff4bf9d009bf0b
Kaden <64388977+Nyionic@users.noreply.github.com>
2021-04-12 19:13:27 -0400

    Add files via upload

---
+++ Veggies-updated/updateplant.php
@@ -0,0 +1,29 @@
+<?php
+$servername = "127.0.0.1";
+$username = "root";
+$password = "mysql";
+$dbname = "veggies";
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+ die("Connection failed: " . mysqli_connect_error());
+}
+
+
+session_start();
+$ret = $_SESSION['row'];
+$sql = "UPDATE plants SET pName = '$_POST[pName]' , pAmnt = '$_POST[pAmnt]' , Freq = '$_POST[Freq]' , addit = '$_POST[addit]' WHERE id = '$_POST[id]'";
+
+if($conn->query($sql))
+{
+    header('location:editplant.php');
+}
+else
+{ 
+    echo "Not Updating";
+}
+
+
+exit();
+
+?>
